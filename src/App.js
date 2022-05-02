c6ebdc7c2a8a02972047c65850c5cb48b0ab5830
Lukman Harun <nandes88.ni@gmail.com>
2022-05-02 20:22:08 +0700

    menambahkan route /tambah-data-emiten

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Home from './pages/Home';
 import DownloadLaporanKeuangan from './pages/DownloadLaporanKeuangan';
 import Info from './pages/Info';
+import TambahEmiten from './pages/TambahEmiten';
 export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/download-laporan-keuangan/:kode_emiten/:nama_emiten' element={<DownloadLaporanKeuangan />} />
-            <Route path='/info/:kode_emiten' element={<Info />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/download-laporan-keuangan/:kode_emiten/:nama_emiten' element={<DownloadLaporanKeuangan />} />
+          <Route path='/info/:kode_emiten' element={<Info />} />
+          <Route path='/tambah-data-emiten' element={<TambahEmiten />} />
         </Routes> 
-    </BrowserRouter>
+    </Router>
   )
 }
