e95a7a80b146aab44914c819c2e6681def87fc85
Lukman Harun <nandes88.ni@gmail.com>
2022-06-08 17:20:00 +0700

    memperbaiki halaman DowloadLaporanKeuangan

--- src/pages/DownloadLaporanKeuangan.js
+++ src/pages/DownloadLaporanKeuangan.js
@@ -1,48 +1,95 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import Table from '../component/Table';
-import Heading from '../component/Heading';
-import TableDownload from '../component/TableDownload';
-import { SERVICE_LAPORAN_KEUANGAN } from '../config';
-import IconHome from '../component/IconHome';
-import IconInfo from '../component/IconInfo';
-import Button from '../component/Button';
-const dataThead = [
-  'No',
-  'Nama File',
-  'Jenis Laporan',
-  'Tanggal',
-  'Download'
-];
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { SERVICE_LAPORAN_KEUANGAN } from "../config";
+import { optionsTahun } from "../constant/optionsTahun";
+import { optionsJenisLaporan } from "../constant/optionsJenisLaporan";
+import axios from "axios";
+import Table from "../component/Table";
+import Heading from "../component/Heading";
+import TableDownload from "../component/TableDownload";
+import IconHome from "../component/IconHome";
+import IconInfo from "../component/IconInfo";
+import Button from "../component/Button";
+import Dropdown from "../component/Dropdown";
+import formatTanggalLaporanKeuangan from "../helper/formatTanggalLaporanKeuangan";
+const dataThead = ["Nama File", "Download"];
 
 export default function DownloadLaporanKeuangan() {
-  let params = useParams();
-  const { kode_emiten, nama_emiten } = params;
+  const { kode_emiten, nama_emiten } = useParams();
   document.title = `Download Laporan Keuangan ${kode_emiten} | ${nama_emiten}`;
-  const [dataLaporanKeuangan, setDataLaporanKeuangan] = useState([]);
+  const [jenisLaporan, setJenisLaporan] = useState(
+    optionsJenisLaporan[0].value
+  );
+  const [tahun, setTahun] = useState(optionsTahun[0].value);
+
+  const [dataLaporanKeuangan, setDataLaporanKeuangan] = useState({});
+  const [tanggal, setTanggal] = useState(
+    formatTanggalLaporanKeuangan(jenisLaporan, tahun)
+  );
+
+  function handleOptionsJenisLaporan(e) {
+    const { value } = e.target;
+    setJenisLaporan(value);
+    setTanggal(formatTanggalLaporanKeuangan(value, tahun));
+  }
+
+  function handleOptionsTahun(e) {
+    const { value } = e.target;
+    setTahun(value);
+    setTanggal(formatTanggalLaporanKeuangan(jenisLaporan, value));
+  }
+
   useEffect(() => {
     // ambil data laporan keuangan
     async function fetchLaporanKeuangan() {
-    const laporanKeuangan = await axios.get(`${SERVICE_LAPORAN_KEUANGAN}/laporan-keuangan/${kode_emiten}`);
-    setDataLaporanKeuangan(laporanKeuangan.data.data);
+      try {
+        const laporanKeuangan = await axios.get(
+          `${SERVICE_LAPORAN_KEUANGAN}/laporan-keuangan/${kode_emiten}/${tanggal}`
+        );
+        setDataLaporanKeuangan(laporanKeuangan.data.data);
+      } catch (error) {
+        setDataLaporanKeuangan({});
+      }
     }
     fetchLaporanKeuangan();
-  }, [kode_emiten]);
+  }, [kode_emiten, tanggal]);
   return (
     <div className="container mt-3">
-      <Heading Tag='h1' className='text-center'>Download Laporan Keuangan</Heading>
-      <Heading Tag='h3' className='text-center mt-3'>{nama_emiten} ({kode_emiten})</Heading>
+      <Heading Tag="h1" className="text-center">
+        Download Laporan Keuangan
+      </Heading>
+      <Heading Tag="h3" className="text-center mt-3">
+        {nama_emiten} ({kode_emiten})
+      </Heading>
       <div className="flex justify-center mt-5 relative">
-        <Button type='link' href='/'>
+        <Button type="link" href="/">
           <IconHome />
         </Button>
-        <Button type='link' href={`/info/${kode_emiten}`}>
+        <Button type="link" href={`/info/${kode_emiten}`}>
           <IconInfo />
         </Button>
       </div>
       <div className="flex justify-center mt-3">
-        <Table dataThead={dataThead} classTr='bg-green-500 text-white' classTh='p-2' classTable='text-center'>
+        {/* dropdown jenis laporan */}
+        <Dropdown
+          options={optionsJenisLaporan}
+          onChange={handleOptionsJenisLaporan}
+          className="w-36 mr-3"
+        />
+        {/* dropdown tahun */}
+        <Dropdown
+          options={optionsTahun}
+          onChange={handleOptionsTahun}
+          className="w-36 ml-3"
+        />
+      </div>
+      <div className="flex justify-center mt-3">
+        <Table
+          dataThead={dataThead}
+          classTr="bg-green-500 text-white"
+          classTh="p-2"
+          classTable="text-center"
+        >
           <TableDownload data={dataLaporanKeuangan} />
         </Table>
       </div>
