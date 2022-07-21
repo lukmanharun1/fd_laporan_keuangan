6766a76d977d0dd0b734762cfc72f29b38e85866
Lukman Harun <nandes88.ni@gmail.com>
2022-07-21 16:33:35 +0700

    menambahkan routes register

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React from "react";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
-import Home from './pages/Home';
-import DownloadLaporanKeuangan from './pages/DownloadLaporanKeuangan';
-import Info from './pages/Info';
-import TambahEmiten from './pages/TambahEmiten';
-import TambahLaporanKeuangan from './pages/TambahLaporanKeuangan';
+import Home from "./pages/Home";
+import DownloadLaporanKeuangan from "./pages/DownloadLaporanKeuangan";
+import Info from "./pages/Info";
+import TambahEmiten from "./pages/TambahEmiten";
+import TambahLaporanKeuangan from "./pages/TambahLaporanKeuangan";
+
+import Register from "./pages/Register";
 export default function App() {
   return (
     <Router>
       <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/download-laporan-keuangan/:kode_emiten/:nama_emiten' element={<DownloadLaporanKeuangan />} />
-          <Route path='/info/:kode_emiten' element={<Info />} />
-          <Route path='/tambah-data-emiten' element={<TambahEmiten />} />
-          <Route path='/tambah-data-laporan-keuangan/:kode_emiten' element={<TambahLaporanKeuangan />} /> 
-        </Routes> 
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/download-laporan-keuangan/:kode_emiten/:nama_emiten"
+          element={<DownloadLaporanKeuangan />}
+        />
+        <Route path="/info/:kode_emiten" element={<Info />} />
+        <Route path="/tambah-data-emiten" element={<TambahEmiten />} />
+        <Route
+          path="/tambah-data-laporan-keuangan/:kode_emiten"
+          element={<TambahLaporanKeuangan />}
+        />
+
+        <Route path="/auth/register" element={<Register />} />
+      </Routes>
     </Router>
-  )
+  );
 }
