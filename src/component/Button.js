1709120a45b768e65bcb5c01e8a5d0c1e38eaf9e
Lukman Harun <nandes88.ni@gmail.com>
2022-08-07 16:41:25 +0700

    memperbaiki component Button

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import propTypes from 'prop-types';
+import React from "react";
+import { Link } from "react-router-dom";
+import propTypes from "prop-types";
 export default function Button(props) {
-  const { type, href, target, onClick, isExternal, isPrimary, children } = props;
+  const {
+    type,
+    href,
+    target,
+    onClick,
+    isExternal,
+    isPrimary,
+    disabled,
+    children,
+  } = props;
   const className = [props.className];
   if (isPrimary) {
-    className.push('p-2 text-white bg-green-500 hover:bg-green-600 rounded-sm');
+    className.push("p-2 text-white bg-green-500 hover:bg-green-600 rounded-sm");
   }
-  if (type === 'link') {
+  if (type === "link") {
     if (isExternal) {
       return (
-        <a 
+        <a
           href={href}
-          className={className.join(' ')}
+          className={className.join(" ")}
           target={target === "_blank" ? "_blank" : undefined}
         >
           {children}
         </a>
-      )
+      );
     } else {
       return (
-        <Link
-          to={href}
-          className={className.join(' ')}
-          onClick={onClick}
-        >
+        <Link to={href} className={className.join(" ")} onClick={onClick}>
           {children}
         </Link>
       );
@@ -33,19 +38,22 @@ export default function Button(props) {
   return (
     <button
       type={type}
-      className={className.join(' ')}
-      onClick={onClick}>
+      className={className.join(" ")}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {children}
     </button>
   );
 }
 
 Button.propTypes = {
-  type: propTypes.oneOf(['button', 'link', 'submit', 'reset']),
+  type: propTypes.oneOf(["button", "link", "submit", "reset"]),
   className: propTypes.string,
   href: propTypes.string,
   target: propTypes.string,
   onClick: propTypes.func,
   isExternal: propTypes.bool,
-  isPrimary: propTypes.bool
-}
+  isPrimary: propTypes.bool,
+  disabled: propTypes.bool,
+};
