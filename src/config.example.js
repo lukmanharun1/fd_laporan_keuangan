7cebb0c3c4f72a261c321a9002649e2278647173
Lukman Harun <nandes88.ni@gmail.com>
2022-08-30 21:33:22 +0700

    menambahkan config REACT_APP_HOST

--- src/config.example.js
+++ src/config.example.js
@@ -1,4 +1,5 @@
 // create file src/config.js
 module.exports = {
   SERVICE_LAPORAN_KEUANGAN: "host:port", // https://github.com/lukmanharun1/laporan_keuangan
+  REACT_APP_HOST: "http://localhost:3000",
 };
